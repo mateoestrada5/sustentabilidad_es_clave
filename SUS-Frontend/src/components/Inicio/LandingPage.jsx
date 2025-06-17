0a762ce2736d5909abcd0466d68d05e72dcf72c5
Juan Cruz Ceballos <94239@sistemas.frc.utn.edu.ar>
2025-06-17 19:56:28 -0300

    pregunta Quien, correciones de SVG divisores, funcionalidad del boton de descarga

--- SUS-Frontend/src/components/Inicio/LandingPage.jsx
+++ SUS-Frontend/src/components/Inicio/LandingPage.jsx
@@ -1,35 +1,32 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import NavbarLP from "./NavbarLP";
-import { MdForest, MdOutlineContactSupport, MdOutlineContactless } from "react-icons/md";
-
-import { HiUserGroup } from "react-icons/hi";
-import { ImBriefcase } from "react-icons/im";
-import { GiRecycle } from 'react-icons/gi';
-import { FaPeopleGroup } from "react-icons/fa6";
-import { GiFactory } from 'react-icons/gi';
-
-import {
-  FaKey,
-  FaLightbulb,
-  FaTools,
-  FaHandHoldingHeart,
-  FaFlagCheckered,
-  FaMoneyBillWave,
-  FaLock,
-} from "react-icons/fa";
-import { RiContactsLine, RiStarLine } from "react-icons/ri";
+import { MdForest, MdOutlineContactSupport, MdOutlineContactless, MdCastForEducation } from "react-icons/md";
+import { GiRecycle, GiFactory } from "react-icons/gi";
+import { FaLightbulb, FaTools, FaHandHoldingHeart, FaFlagCheckered, FaChartLine, FaSchool } from "react-icons/fa";
+import { FaPeopleGroup, FaPerson } from "react-icons/fa6";
+import { RiContactsLine, RiStarLine, RiGovernmentLine } from "react-icons/ri";
 import { BiQuestionMark, BiSupport } from "react-icons/bi";
 import { GoArrowDown } from "react-icons/go";
-import { IoNewspaperOutline } from "react-icons/io5";
 import { PiNutBold } from "react-icons/pi";
-import { FaPerson } from "react-icons/fa6";
-import { MdCastForEducation } from "react-icons/md";
-import { IoMegaphoneOutline } from "react-icons/io5";
+import { IoMegaphoneOutline, IoPeople, IoNewspaperOutline } from "react-icons/io5";
 import { VscTools } from "react-icons/vsc";
 import { LiaUtensilsSolid } from "react-icons/lia";
+import { IoMdBusiness } from "react-icons/io";
+
+
+
+
 const LandingPage = () => {
   const [activeCard, setActiveCard] = useState(null); // Estado para controlar qué tarjeta está activa
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/Informe-Sustentabilidad-2025.pdf';
+    link.download = 'Informe-Sustentabilidad-2025.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   // colores:
   // white
@@ -44,7 +41,7 @@ const LandingPage = () => {
   // M0,128L80,106.7C160,85,320,43,480,64C640,85,800,181,960,170.7C1120,160,1280,64,1360,42.7L1440,21L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z
   // M0,192L40,186.7C80,181,160,171,240,154.7C320,139,400,117,480,128C560,139,640,181,720,192C800,203,880,181,960,154.7C1040,128,1120,96,1200,96C1280,96,1360,128,1400,144L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z
 
-  const paraQueActions1 = [
+  const paraQueActions = [
     {
       id: 1,
       title: "Propósito",
@@ -87,7 +84,7 @@ const LandingPage = () => {
     },
   ];
 
-   const costosSustentabilidad = [
+  const costosSustentabilidad = [
     {
       id: 1,
       title: "Inversión Inicial",
@@ -99,7 +96,7 @@ const LandingPage = () => {
           <br />
           de la empresa
         </>
-        
+
       ),
       icon: <FaChartLine style={{ color: "#004d29" }} />,
     },
@@ -115,16 +112,16 @@ const LandingPage = () => {
       ),
       icon: <GoArrowDown style={{ color: "#02523a" }} />,
     },
-        {
+    {
       id: 3,
       title: "Reducción de Contaminación",
       description: (
         <>
           Hasta un 80% del C02
-                    <br />
-                    en algunos casos
+          <br />
+          en algunos casos
         </>
-        
+
       ),
       icon: <GiRecycle style={{ color: "#004d29" }} />,
     },
@@ -133,30 +130,31 @@ const LandingPage = () => {
       title: "Impacto Social",
       description: (
         <>
-          Influencia en 
+          Influencia en
           <br />
           miles de personas
-          
+
         </>
       ),
       icon: <FaPeopleGroup style={{ color: "#02523a" }} />,
     },
-        {
+    {
       id: 5,
       title: "Contaminación Actual",
       description: (
         <>
           150-400 tn de CO2 al año
           <br />
-          emitida por una planta de producción 
+          emitida por una planta de producción
 
         </>
-        
+
       ),
-      icon: <GiFactory style={{ color: "#004d29" }} />,},
+      icon: <GiFactory style={{ color: "#004d29" }} />,
+    },
 
   ];
-    const recursos = [
+  const recursos = [
     {
       id: 1,
       title: "Instrumentos",
@@ -194,7 +192,7 @@ const LandingPage = () => {
         "• Recursos humanos",
       ],
     },
-        {
+    {
       id: 4,
       title: "Elementos",
       description:
@@ -207,7 +205,7 @@ const LandingPage = () => {
         "• Marcos legales",
       ],
     },
-        {
+    {
       id: 5,
       title: "Medios",
       description:
@@ -220,7 +218,7 @@ const LandingPage = () => {
         "• Movimientos sociales",
       ],
     },
-        {
+    {
       id: 6,
       title: "Herramientas",
       description:
@@ -232,7 +230,7 @@ const LandingPage = () => {
         "• Instrumentos de medición y control ambiental",
       ],
     },
-        {
+    {
       id: 7,
       title: "Utensilios",
       description:
@@ -247,7 +245,7 @@ const LandingPage = () => {
     },
   ];
 
-  const cardData = [
+  const cardDataPorQue = [
     {
       id: 1,
       title: "Causa",
@@ -271,6 +269,44 @@ const LandingPage = () => {
         "Necesitamos urgentemente  replantear la forma en que producimos, consumimos y nos relacionamos con el entorno, para garantizar un equilibrio entre el desarrollo humano y la conservación de los recursos.",
       icon: <MdForest className="w-16 h-16 mb-4" />,
       functions: [],
+    },
+  ];
+
+  const cardDataQuien = [
+    {
+      id: 1,
+      title: "Gobiernos",
+      description:
+        "Crean y aplican marcos legales y políticas públicas ambientales que regulan una sociedad.",
+      icon: <RiGovernmentLine className="w-16 h-16 mb-4" />,
+    },
+    {
+      id: 2,
+      title: "Empresas",
+      description:
+        "Responden y adaptan sus procesos para respetar las normas ambientales. Innovan en tecnologías limpias, realizan reportes de su impacto, y buscan reducir la huella ecológica.",
+      icon: <IoMdBusiness className="w-16 h-16 mb-4" />,
+    },
+    {
+      id: 3,
+      title: "Ciudadanos",
+      description:
+        "Practican el consumo consciente, participan activamente en la defensa ambiental, y exigen transparencia de consumo a los gobiernos y empresas.",
+      icon: <IoPeople className="w-16 h-16 mb-4" />,
+    },
+    {
+      id: 4,
+      title: "ONGs y Activistas",
+      description:
+        "Denuncian ecocidios, educan comunidades y presionan a los tomadores de decisiones mediante campañas y litigios.",
+      icon: <MdForest className="w-16 h-16 mb-4" />,
+    },
+    {
+      id: 5,
+      title: "Educadores y Medios de Comunicación",
+      description:
+        "Transmiten buenas prácticas y alertan sobre problemas ambientales para generar conciencia en la sociedad.",
+      icon: <FaSchool className="w-16 h-16 mb-4" />,
     },
   ];
 
@@ -410,12 +446,12 @@ const LandingPage = () => {
         className="min-h-screen flex flex-col items-center justify-center py-16 px-8 text-center"
         id="por_que"
       >
-        <h2 className="text-3xl font-semibold mb-6">¿Por qué sustentabilidad?</h2>
+        <h2 className="text-3xl font-semibold mb-6">¿Por qué surge la sustentabilidad?</h2>
         <p className="text-lg mb-10">
           Es una reacción a los efectos perjudiciales del desarrollo insostenible.
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-[1200px]">
-          {cardData.map((card) => (
+          {cardDataPorQue.map((card) => (
             <div
               key={card.id}
               className="relative bg-custom-green text-white rounded-lg shadow-lg p-6 transition-transform duration-300 transform hover:scale-105 cursor-pointer h-72"
@@ -458,21 +494,17 @@ const LandingPage = () => {
         id="para_que"
       >
         <h2 className="text-3xl font-semibold mb-6">
-          ¿Para qué la sustentabilidad?
+          ¿Para qué sirve la sustentabilidad?
         </h2>
         <p className="text-lg mb-6">
           La sustentabilidad no es solo una meta, sino una necesidad urgente y compartida
         </p>
         <div className="flex justify-center">
           <div className="grid grid-cols-4 gap-10">
-            {paraQueActions1.map((action) => (
+            {paraQueActions.map((action) => (
               <div
                 key={action.id}
-                className="bg-white rounded-lg shadow-lg p-6 text-center transition-transform transform hover:scale-105 min-h-[200px] flex flex-col justify-between"
-                style={{
-                  border: "2px solid #004d29",
-                  borderRadius: "8px",
-                }}
+                className="bg-white rounded-lg shadow-lg p-6 text-center transition-transform transform hover:scale-105 min-h-[200px] flex flex-col justify-between border-2 border-custom-green"
               >
                 <div className="flex justify-center items-center mb-4 text-4xl">
                   {action.icon}
@@ -484,72 +516,49 @@ const LandingPage = () => {
           </div>
         </div>
         <br />
-        {/* <div className="flex justify-center w-full">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-            {paraQueActions2.map((action) => (
-              <div
-                key={action.id}
-                className="bg-white rounded-lg shadow-lg p-6 text-center transition-transform transform hover:scale-105 min-h-[200px] flex flex-col justify-between"
-                style={{
-                  border: "2px solid #004d29",
-                  borderRadius: "8px",
-                }}
-              >
-                <div className="flex justify-center items-center mb-4 text-4xl">
-                  {action.icon}
-                </div>
-                <h3 className="font-semibold text-lg">{action.title}</h3>
-                <p className="text-gray-600">{action.description}</p>
-              </div>
-            ))}
-          </div>
-        </div> */}
       </section>
 
       <MountainDivider backgroundColor="#f3f4f6" />
 
       {/* quien */}
       <section
-        className="min-h-screen flex flex-col items-center justify-center py-16 px-8 text-center bg-custom-green"
+        className="min-h-screen flex flex-col items-center justify-center py-16 px-8 text-center bg-custom-green text-white"
         id="quien"
       >
-        <h2 className="text-3xl font-semibold mb-6">¿Quién?</h2>
+        <h2 className="text-3xl font-semibold mb-6">¿Quién es responsable de la sustentabilidad?</h2>
         <p className="text-lg mb-10">
-          Es una reacción a los efectos perjudiciales del desarrollo insostenible.
+          La responsabilidad es colectiva y compartida. Todos en la socidad participan y asumen un rol para preservar el planeta.
         </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-[1200px]">
-          {cardData.map((card) => (
+        <div className="grid grid-cols-3 gap-8 w-full max-w-[1200px]">
+          {cardDataQuien.map((card) => (
             <div
               key={card.id}
-              className="relative bg-custom-green text-white rounded-lg shadow-lg p-6 transition-transform duration-300 transform hover:scale-105 cursor-pointer h-72"
+              className="relative bg-slate-100 text-black rounded-xl shadow-lg p-2 transition-transform duration-300 transform hover:scale-105 cursor-pointer h-72"
               onClick={() =>
                 setActiveCard(activeCard === card.id ? null : card.id)
               }
             >
-              <div className="flex flex-col items-center justify-center h-full">
-                {activeCard === card.id ? (
-                  <>
-                    <h3 className="text-xl font-semibold">{card.title}</h3>
-                    <p className="text-lg text-center">{card.description}</p>
-                    <ul className="mt-4 text-left">
-                      {card.functions.map((func, index) => (
-                        <li key={index}>{func}</li>
-                      ))}
-                    </ul>
-                  </>
-                ) : (
-                  <>
-                    {card.icon}
-                    <h3 className="text-xl font-semibold">{card.title}</h3>
-                    <p className="text-lg text-center">Haz clic para ver más</p>
-                  </>
-                )}
+              <div className="border-2 border-custom-green h-full rounded-xl">
+                <div className="flex flex-col items-center justify-center h-full p-4">
+                  {activeCard === card.id ? (
+                    <>
+                      <h3 className="text-xl font-semibold ">{card.title}</h3>
+                      <p className="text-lg text-center">{card.description}</p>
+                    </>
+                  ) : (
+                    <>
+                      {card.icon}
+                      <h3 className="text-xl font-semibold ">{card.title}</h3>
+                      <p className="text-lg text-center">Haz clic para ver más</p>
+                    </>
+                  )}
+                </div>
               </div>
             </div>
           ))}
         </div>
         <hr class="border-t-2 border-gray-300 my-6 w-[90%] mx-auto mt-10" />
-        <p className="text-lg  italic">La sustentabilidad no es una opción, sino una necesidad urgente para asegurar un futuro habitable, protegiendo el planeta y garantizando bienestar para las próximas generaciones.</p>
+        <p className="text-lg  italic">Toda la sociedad está afectada por las decisiones y acciones relacionadas con la sustentabilidad. El impacto es global y abarca a las generaciones presentes y futuras.</p>
         <hr class="border-t-2 border-gray-300 my-6 w-[90%] mx-auto" />
       </section>
 
@@ -565,26 +574,49 @@ const LandingPage = () => {
 
       {/* cuando */}
       <section
-        className="py-16 px-8 bg-custom-green flex flex-col md:flex-row"
+        className="py-16 px-8 bg-white flex flex-col min-h-screen"
         id="cuando"
       >
+        cuando
       </section>
+
+      <WaveDivider
+        color={["#397559", "#02523a"]}
+        backgroundColor="white"
+        rotate={false}
+        svgPaths={[
+          "M0,128L80,106.7C160,85,320,43,480,64C640,85,800,181,960,170.7C1120,160,1280,64,1360,42.7L1440,21L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z",
+          "M0,256L80,229.3C160,203,320,149,480,149.3C640,149,800,203,960,213.3C1120,224,1280,192,1360,186.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z",
+        ]}
+      />
+
 
       {/* donde */}
       <section
-        className="py-16 px-8 bg-custom-green flex flex-col md:flex-row"
+        className="py-16 px-8 bg-custom-green flex flex-col min-h-screen text-white"
         id="donde"
       >
-
+        donde
       </section>
+
+      <WaveDivider
+        color={["#c6d7cf", "#02523a"]}
+        backgroundColor={"#f3f4f6"}
+        rotate={true}
+        svgPaths={[
+          "M1440,128L1360,106.7C1280,85,1120,43,960,64C800,85,640,181,480,170.7C320,160,160,64,80,42.7L0,21L0,320L80,320C160,320,320,320,480,320C640,320,800,320,960,320C1120,320,1280,320,1360,320L1440,320Z",
+          "M0,160C80,120,160,80,240,64C320,48,400,64,480,96C560,128,640,176,720,192C800,208,880,176,960,160C1040,144,1120,128,1200,160C1280,192,1360,256,1440,320L1440,320L0,320Z",
+        ]}
+      />
 
       {/* como */}
       <section
-        className="py-16 px-8 bg-custom-green flex flex-col md:flex-row"
+        className="py-16 px-8 bg-gray-100 flex flex-col min-h-screen"
         id="como"
       >
+        como
       </section>
-      <WaveDivider color={["#f3f4f6"]} backgroundColor="white" rotate={false} />
+      <WaveDivider color={["#f3f4f6"]} backgroundColor="white" rotate={true} />
 
       {/* con que */}
       <section
@@ -625,10 +657,8 @@ const LandingPage = () => {
           ))}
         </div>
       </section>
-      <MountainDivider backgroundColor="#f3f4f6" />
 
       <WaveDivider color={["#f3f4f6"]} backgroundColor="white" rotate={false} />
-
 
       {/* cuanto */}
       <section
@@ -638,7 +668,7 @@ const LandingPage = () => {
         <h2 className="text-3xl font-semibold mb-6">
           ¿Cuánto cuesta y se gana siendo sustentable?
         </h2>
-                  <br />
+        <br />
         <div className="flex justify-center">
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 grid-cols-[auto-fit] ">
             {costosSustentabilidad.map((action) => (
@@ -661,26 +691,37 @@ const LandingPage = () => {
         </div>
         <br />
       </section>
-      <MountainDivider backgroundColor="#f3f4f6" />
+
 
       <WaveDivider
-        color={["#c6d7cf", "#02523a"]}
+        color={["#f3f4f6"]}
         backgroundColor="white"
         rotate={true}
         svgPaths={[
-          "M1440,128L1360,106.7C1280,85,1120,43,960,64C800,85,640,181,480,170.7C320,160,160,64,80,42.7L0,21L0,320L80,320C160,320,320,320,480,320C640,320,800,320,960,320C1120,320,1280,320,1360,320L1440,320Z",
-          "M0,160C80,120,160,80,240,64C320,48,400,64,480,96C560,128,640,176,720,192C800,208,880,176,960,160C1040,144,1120,128,1200,160C1280,192,1360,256,1440,320L1440,320L0,320Z",
+          "M0,160C80,140,160,130,240,140C320,150,400,160,480,165C560,170,640,180,720,185C800,190,880,180,960,170C1040,160,1120,150,1200,160C1280,170,1360,190,1440,200L1440,320L0,320Z", ,
         ]}
       />
 
+      {/* Boton de descarga */}
       <section
-        className="min-h-[30%] flex flex-col lg:flex-row items-center justify-center py-16 px-8 bg-white text-center"
+        className="min-h-[30%] py-16 px-8 bg-white text-center"
         id="contacto">
-        <button className="bg-custom-green p-4 m-2 text-white font-bold rounded-xl shadow-xl hover:scale-[1.2] transition transition-duration-200">Descarga el informe completo</button>
+        <hr class="border-t-2 border-gray-300 mb-20 w-[90%] mx-auto" />
+
+        <div className="items-center justify-center p-30">
+          <p className="text-lg italic pb-5">¿Querés profundizar en alguna de estas preguntas? Te invitamos a explorar el siguiente informe sobre la sustentabilidad, donde desarrollamos cada tema con más detalle.</p>
+          <button
+            className="bg-custom-green p-7 m-2 text-white text-lg font-bold rounded-full shadow-2xl hover:scale-[1.2] transition transition-duration-200"
+            onClick={handleDownload}>
+            Descarga el informe completo</button>
+        </div>
+
+        <hr class="border-t-2 border-gray-300 mt-[5%] mx-auto w-[90%] " />
       </section>
 
+
       <section
-        className="min-h-screen flex flex-col lg:flex-row items-center justify-center py-16 px-8 bg-white text-center"
+        className="min-h-screen flex flex-col lg:flex-row items-center justify-center bg-white text-center"
         id="contacto"
       >
         <div className="flex-1 flex justify-center mt-8">
@@ -730,7 +771,7 @@ const LandingPage = () => {
         </div>
       </section>
 
-      <MountainDivider color={["#c6d7cf", "#397559", "#004d29"]} />
+      <MountainDivider color={["#c6d7cf", "#397559", "#02523a"]} />
     </div>
   );
 };
